d3982250cf6ba0b5eecfd5c29e946b138fff7981
Jai Sinha <sinhajai002@gmail.com>
2023-03-09 17:16:57 +1030

    commit msg

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import AllRoutes from "./AllRoutes";
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Auth from "./pages/auth/auth";
+import Login from "./components/userpage/Login";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar></Navbar>
-            <AllRoutes/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <AllRoutes />
+    </div>
+  );
 }
 
 export default App;
